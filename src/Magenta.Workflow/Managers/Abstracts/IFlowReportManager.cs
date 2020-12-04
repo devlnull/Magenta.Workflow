aff6d8b0021d5efe1f51f33ce67bd2b60ecdb2d8
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-12-04 22:28:57 +0330

    Add base abstracts and concrete

---
+++ src/Magenta.Workflow/Managers/Abstracts/IFlowReportManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magenta.Workflow.Managers.Abstracts
+{
+    public interface IFlowReportManager
+    {
+    }
+}
---
+++ src/Magenta.Workflow/Managers/Abstracts/IFlowSecurityContext{T}.cs
@@ -0,0 +1,29 @@
+﻿using Magenta.Workflow.Managers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magenta.Workflow.Managers.Abstracts
+{
+    public interface IFlowSecurityContext<TKey>
+    where TKey : IComparable
+    {
+        #region Get
+        #region User
+        TKey GetUserId();
+        Task<TKey> GetUserIdAsync();
+        Task<TKey> GetAutomationUserIdAsync();
+        Task<Dictionary<string, string>> GetUsernameAndIdByIdAsync(IEnumerable<string> userIds);
+        #endregion
+        #region Role
+        Task<IEnumerable<TKey>> GetUserRoleIdsAsync();
+        #endregion
+        #endregion
+
+        #region Filter Access Cartable
+        Task<IQueryable<FlowCartableModel>> FilterAccessCartableAsync(IQueryable<FlowCartableModel> query);
+        #endregion
+    }
+
+}
