96c5040cffcbcc503075a584084e51c336fc1ab8
Amirhossein Ghorbani <amirhosseinghorbani@hotmail.com>
2020-12-04 23:39:58 +0330

    Add flow set and state manager to handle the state of flows, implement in memory state manager.

---
+++ src/Magenta.Workflow/Managers/States/Abstracts/IStateManager.cs
@@ -0,0 +1,9 @@
+﻿using Magenta.Workflow.DataTransferObjects.Base;
+
+namespace Magenta.Workflow.Managers.States.Abstracts
+{
+    public interface IStateManager
+    {
+        IFlowSet<TEntity> GetFlowSet<TEntity>() where TEntity : FlowEntity;
+    }
+}
