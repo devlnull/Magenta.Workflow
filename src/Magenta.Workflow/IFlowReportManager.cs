48c6b58a189dab6ce06f3d23d7fc5078c6ac2a0c
Amirhossein Ghorbani <Am.Ghorbani@Alibaba.ir>
2021-05-30 04:32:31 +0430

    Add two report functionality to get all steps and current tranistions of an instance

--- src/Magenta.Workflow/IFlowReportManager.cs
+++ src/Magenta.Workflow/IFlowReportManager.cs
@@ -1,6 +1,28 @@
-﻿namespace Magenta.Workflow
+﻿using Magenta.Workflow.Context.Flows;
+using Magenta.Workflow.Core.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Magenta.Workflow
 {
     public interface IFlowReportManager
     {
+        #region Transitions
+        /// <summary>
+        /// Get instance current transitions based on current instance state
+        /// </summary>
+        /// <returns>all possible transitions</returns>
+        Task<FlowResult<IEnumerable<FlowTransition>>> GetInstanceTransitionsAsync(Guid instanceId);
+        #endregion Transitions
+
+        #region Steps
+        /// <summary>
+        /// Get list of all steps that an instance has taken
+        /// </summary>
+        /// <param name="instanceId"></param>
+        /// <returns>list of steps</returns>
+        Task<FlowResult<IEnumerable<FlowStep>>> GetInstanceStepsAsync(Guid instanceId);
+        #endregion Steps
     }
 }
--- src/Magenta.Workflow/Managers/Flows/FlowReportManager.cs
+++ src/Magenta.Workflow/Managers/Flows/FlowReportManager.cs
@@ -1,6 +1,84 @@
-﻿namespace Magenta.Workflow.Managers.Flows
+﻿using Magenta.Workflow.Context.Flows;
+using Magenta.Workflow.Core.Tasks;
+using Magenta.Workflow.Managers.States;
+using Magenta.Workflow.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Magenta.Workflow.Managers.Flows
 {
-    public class FlowReportManager
+    public class FlowReportManager : IFlowReportManager
     {
+        public FlowReportManager(IStateManager stateManager)
+        {
+            StateManager = stateManager ?? throw new ArgumentNullException(nameof(stateManager));
+        }
+
+        public IStateManager StateManager { get; set; }
+
+
+        public async Task<FlowResult<IEnumerable<FlowTransition>>> GetInstanceTransitionsAsync(Guid instanceId)
+        {
+            //Get current instance
+            if (instanceId.GuidIsEmpty())
+                throw new ArgumentNullException(nameof(instanceId));
+
+            var instanceSet = StateManager.GetFlowSet<FlowInstance>();
+
+            var targetInstance = await instanceSet.FirstOrDefaultAsync(x => x.Id.Equals(instanceId));
+            if (targetInstance == null)
+                return FlowResult<IEnumerable<FlowTransition>>
+                    .Failed(new FlowError(FlowErrors.ITEM_NOTFOUND, args: nameof(FlowInstance)));
+
+            //Get current instance state
+            var stepSet = StateManager.GetFlowSet<FlowStep>();
+
+            var currentStep = await stepSet
+                .FirstOrDefaultAsync(x => x.InstanceId.Equals(targetInstance.Id) && x.IsCurrent);
+
+            if(currentStep == null)
+                return FlowResult<IEnumerable<FlowTransition>>
+                    .Failed(new FlowError(FlowErrors.INSTANCE_HASNOSTEP));
+
+            //Get current step transition
+            var transitionSet = StateManager.GetFlowSet<FlowTransition>();
+
+            var currentTransition = await transitionSet
+                .FirstOrDefaultAsync(x => x.Id.Equals(currentStep.TransitionId));
+
+            //Get current state
+            var stateSet = StateManager.GetFlowSet<FlowState>();
+
+            var currentState = await stateSet.FirstOrDefaultAsync(x => x.Id.Equals(currentTransition.DestinationId));
+
+            //Get current state transitions
+            var transitions = await transitionSet.GetAllAsync(x => x.SourceId.Equals(currentState.Id));
+
+            return FlowResult<IEnumerable<FlowTransition>>.Successful(transitions);
+        }
+
+
+        public async Task<FlowResult<IEnumerable<FlowStep>>> GetInstanceStepsAsync(Guid instanceId)
+        {
+            //Get current instance
+            if (instanceId.GuidIsEmpty())
+                throw new ArgumentNullException(nameof(instanceId));
+
+            var instanceSet = StateManager.GetFlowSet<FlowInstance>();
+
+            var targetInstance = await instanceSet.FirstOrDefaultAsync(x => x.Id.Equals(instanceId));
+            if (targetInstance == null)
+                return FlowResult<IEnumerable<FlowStep>>
+                    .Failed(new FlowError(FlowErrors.ITEM_NOTFOUND, args: nameof(FlowInstance)));
+
+            //Get all steps
+            var stepSet = StateManager.GetFlowSet<FlowStep>();
+            var steps = await stepSet.GetAllAsync(x => x.InstanceId.Equals(targetInstance.Id));
+
+            return FlowResult<IEnumerable<FlowStep>>.Successful(steps);
+        }
     }
 }
