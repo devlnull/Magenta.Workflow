27641afb571d39d24763cf137122ca8311754782
Amirhossein Ghorbani <Am.Ghorbani@Alibaba.ir>
2021-06-10 21:18:25 +0430

    Add get paged flow types in flow report managers

--- src/Magenta.Workflow/Managers/Reports/FlowTypeReportManager.cs
+++ src/Magenta.Workflow/Managers/Reports/FlowTypeReportManager.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Magenta.Workflow.Context.Flows;
 using Magenta.Workflow.Core.Tasks;
@@ -9,7 +12,6 @@ namespace Magenta.Workflow.Managers.Reports
 {
     public partial class FlowReportManager
     {
-
         public async Task<FlowResult<FlowType>> GetTypeByIdAsync(Guid id)
         {
             try
@@ -61,5 +63,85 @@ namespace Magenta.Workflow.Managers.Reports
             }
         }
 
+        public async Task<FlowResult<IEnumerable<FlowType>>> GetTypesAsync()
+        {
+            try
+            {
+                Logger.LogInfo("try to get list of types.");
+                var typeSet = StateManager.GetFlowSet<FlowType>();
+                var types = await typeSet.GetAllAsync();
+                var result = new FlowResult<IEnumerable<FlowType>>();
+                result.SetResult(types);
+                Logger.LogInfo($"list of types with count '{types.Count()}' fetched.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return FlowResult<IEnumerable<FlowType>>.Failed(new FlowError(ex.Message));
+            }
+        }
+
+        public async Task<FlowResult<PagedList<FlowType>>> GetPagedTypesAsync(PageOptions pageOptions)
+        {
+            try
+            {
+                Logger.LogInfo("try to get paged list of types.");
+                var typeSet = StateManager.GetFlowSet<FlowType>();
+                var pagedList = await typeSet.GetPagedAllAsync(pageOptions: pageOptions);
+                var result = new FlowResult<PagedList<FlowType>>();
+                result.SetResult(pagedList);
+                Logger.LogInfo($"paged list of types with count '{pagedList.Count}' fetched.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return FlowResult<PagedList<FlowType>>.Failed(new FlowError(ex.Message));
+            }
+        }
+
+        public async Task<FlowResult<IEnumerable<FlowType>>> GetTypesByEntityAsync(Type entityType)
+        {
+            try
+            {
+                Logger.LogInfo($"try to get list of types by entity '{entityType.FullName}'.");
+                var typeSet = StateManager.GetFlowSet<FlowType>();
+                var types = await typeSet
+                    .GetAllAsync(x => x.EntityType.Equals(entityType.FullName));
+                var result = new FlowResult<IEnumerable<FlowType>>();
+                result.SetResult(types);
+                Logger.LogInfo($"list of types by entity '{entityType.FullName}'" +
+                               $" with count '{types.Count()}' fetched.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return FlowResult<IEnumerable<FlowType>>.Failed(new FlowError(ex.Message));
+            }
+        }
+
+        public async Task<FlowResult<PagedList<FlowType>>> GetPagedTypesByEntityAsync(Type entityType,
+            PageOptions pageOptions)
+        {
+            try
+            {
+                Logger.LogInfo("try to get paged list of types.");
+                var typeSet = StateManager.GetFlowSet<FlowType>();
+                var pagedList = await typeSet.GetPagedAllAsync(
+                    predicate: x=>x.EntityType.Equals(entityType.FullName),
+                    pageOptions: pageOptions);
+                var result = new FlowResult<PagedList<FlowType>>();
+                result.SetResult(pagedList);
+                Logger.LogInfo($"paged list of types with count '{pagedList.Count}' fetched.");
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                return FlowResult<PagedList<FlowType>>.Failed(new FlowError(ex.Message));
+            }
+        }
     }
 }
